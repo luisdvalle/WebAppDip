f5fb0e37b7c31277d101695788bc37b966ab867d
luisdvalle <jums.delvalle@gmail.com>
2018-03-22 21:18:29 +1100

    Product and CategoryDetailsViewModel

---
+++ PracticeTestTafe/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeTestTafe.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ PracticeTestTafe/ViewModels/CategoryDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeTestTafe.ViewModels
+{
+    public class CategoryDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public int Total { get; set; }
+    }
+}
