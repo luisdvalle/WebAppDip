466b2fe5648e6f8aafe4c7e9a7a5f76930ffb2f4
luisdvalle <jums.delvalle@gmail.com>
2018-03-22 16:56:12 +1100

    Adding HomeCategory VM

---
+++ PracticeTestTafe/ViewModels/HomeCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PracticeTestTafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeTestTafe.ViewModels
+{
+    public class HomeCategoryViewModel
+    {
+        public int Total { get; set; }
+        public IEnumerable<Category> CategoryList { get; set; }
+    }
+}
