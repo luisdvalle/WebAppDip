bf7efa5067bf33abe954c65168b0aeee017343d7
luisdvalle <jums.delvalle@gmail.com>
2018-03-22 16:07:55 +1100

    Implementing DbContext and adding migration

---
+++ PracticeTestTafe/Migrations/20180322050106_categoryTblMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PracticeTestTafe.Migrations
+{
+    public partial class categoryTblMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryTbl",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Details = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryTbl", x => x.CategoryId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryTbl");
+        }
+    }
+}
---
+++ PracticeTestTafe/Services/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeTestTafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeTestTafe.Services
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Category> CategoryTbl { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options
+                .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB ; Database=PracticeTestDB ; Trusted_Connection=True");
+        }
+    }
+}
